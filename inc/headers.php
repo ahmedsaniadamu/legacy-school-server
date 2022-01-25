2ca5fbc9eb330eddf5a010816d1b347406e435a3
ahmed sani adamu <saniahme813@gmail.com>
2022-01-25 06:04:52 +0100

    allow specific url to access this server

--- inc/headers.php
+++ inc/headers.php
@@ -1,6 +1,6 @@
 <?php
   // modifying headers to  allow all origin domains to access and use this API.
-      header('Access-Control-Allow-Origin:*');
+      header('Access-Control-Allow-Origin:https://legacysecondaryschool.netlify.app');
       header('Access-Control-Allow-Methods:GET,POST') ;
       header('Access-Control-Allow-Headers:Content-Type') ;
       header('Content-Type:application/json')
